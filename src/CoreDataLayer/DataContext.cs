9fcfa36ac42fbf94ca6f2097de33ab87783710df
Callum Evans <callumevans@outlook.com>
2016-07-31 22:41:44 +0100

    Initial commit

---
+++ src/CoreDataLayer/DataContext.cs
@@ -0,0 +1,45 @@
+﻿using Dapper;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Common;
+
+namespace CoreDataLayer
+{
+    public class DataContext<ConnectionType> : IDataContext 
+        where ConnectionType : DbConnection
+    {
+        public DbConnection Connection { get; private set; }
+
+        public DataContext(string connectionString)
+        {
+            this.Connection = (DbConnection)Activator.CreateInstance(typeof(ConnectionType), connectionString);
+        }
+
+        public IEnumerable<T> ExecuteReadProcedure<T>(string procedure, object parameters)
+        {
+            var inputs = new DynamicParameters();
+
+            if (parameters != null)
+            {
+                inputs.AddDynamicParams(parameters);
+            }
+
+            var results = Connection.Query<T>(procedure, inputs, commandType: CommandType.StoredProcedure);
+            return results;
+        }
+
+        public int ExecuteWriteProcedure(string procedure, object parameters)
+        {
+            var inputs = new DynamicParameters();
+
+            if (parameters != null)
+            {
+                inputs.AddDynamicParams(parameters);
+            }
+
+            int index = Connection.Execute(procedure, inputs, commandType: CommandType.StoredProcedure);
+            return index;
+        }
+    }
+}
---
+++ src/CoreDataLayer/IDataContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreDataLayer
+{
+    public interface IDataContext
+    {
+        DbConnection Connection { get; }
+
+        int ExecuteWriteProcedure(string procedure, object parameters = null);
+
+        IEnumerable<T> ExecuteReadProcedure<T>(string procedure, object parameters = null);
+    }
+}
