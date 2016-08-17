09255723102abd91cbc0254c4ddbacff06555621
Callum Evans <callumevans@outlook.com>
2016-08-17 23:31:15 +0100

    Integration tests

---
+++ test/CoreDataLayer.Tests/DataContextSqlServerTests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace CoreDataLayer.Tests
+{
+    public class DataContextSqlServerTests
+    {
+        [Fact]
+        public void ConnectToSqlServer()
+        {
+            IDataContext context = new DataContext<SqlConnection>(@"Server=(local)\SQL2016;Database=master;User ID=sa;Password=Password12!");
+
+            context.Connection.Open();
+        }
+    }
+}
