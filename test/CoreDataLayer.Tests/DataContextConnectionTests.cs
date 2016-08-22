a5cd93ded9bc9bcc2bf75df650f9f5f600ac2e05
Callum Evans <callumevans@outlook.com>
2016-08-22 03:09:11 +0100

    More thorough tests

--- test/CoreDataLayer.Tests/DataContextConnectionTests.cs
+++ test/CoreDataLayer.Tests/DataContextConnectionTests.cs
@@ -1,5 +1,6 @@
 ﻿using MySql.Data.MySqlClient;
 using Npgsql;
+using System;
 using System.Data.SqlClient;
 using Xunit;
 
@@ -18,6 +19,22 @@ namespace CoreDataLayer.Tests
             context.Connection.Close();
         }
 
+        [Theory]
+        [InlineData(@"Server = (local)\SQL2016;Database = master;User ID = sa;Password = INCORRECT")]
+        [InlineData(@"Server = (local)\SQL2016;Database = master;User ID = INCORRECT;Password = Password12!")]
+        [InlineData(@"Server = (local)\SQL2016;Database = INCORRECT;User ID = sa;Password = Password12!")]
+        [InlineData(@"Server = INCORRECT;Database = master;User ID = sa;Password = Password12!")]
+        public void FailConnectionToSqlServer(string connectionString)
+        {
+            context = new DataContext<SqlConnection>(connectionString);
+
+            Assert.ThrowsAny<Exception>(() =>
+            {
+                context.Connection.Open();
+                context.Connection.Close();
+            });
+        }
+
         [Fact]
         public void ConnectToMySql()
         {
@@ -27,6 +44,22 @@ namespace CoreDataLayer.Tests
             context.Connection.Close();
         }
 
+        [Theory]
+        [InlineData(@"Server=localhost;Database=TestDatabase;Uid=root;Pwd=INCORRECT;")]
+        [InlineData(@"Server=localhost;Database=TestDatabase;Uid=INCORRECT;Pwd=Password12!;")]
+        [InlineData(@"Server=localhost;Database=INCORRECT;Uid=root;Pwd=Password12!;")]
+        [InlineData(@"Server=INCORRECT;Database=TestDatabase;Uid=root;Pwd=Password12!;")]
+        public void FailConnectionToMySql(string connectionString)
+        {
+            context = new DataContext<MySqlConnection>(connectionString);
+
+            Assert.ThrowsAny<Exception>(() =>
+            {
+                context.Connection.Open();
+                context.Connection.Close();
+            });
+        }
+
         [Fact]
         public void ConnectToPostgreSql()
         {
@@ -34,6 +67,23 @@ namespace CoreDataLayer.Tests
 
             context.Connection.Open();
             context.Connection.Close();
+        }
+
+        [Theory]
+        [InlineData(@"User ID=postgres;Password=Password12!;Host=localhost;Port=5432;Database=INCORRECT;")]
+        [InlineData(@"User ID=postgres;Password=Password12!;Host=localhost;Port=INCORRECT;Database=TestDatabase;")]
+        [InlineData(@"User ID=postgres;Password=Password12!;Host=INCORRECT;Port=5432;Database=TestDatabase;")]
+        [InlineData(@"User ID=postgres;Password=INCORRECT;Host=localhost;Port=5432;Database=TestDatabase;")]
+        [InlineData(@"User ID=INCORRECT;Password=Password12!;Host=localhost;Port=5432;Database=TestDatabase;")]
+        public void FailConnectionToPostgreSql(string connectionString)
+        {
+            context = new DataContext<NpgsqlConnection>(connectionString);
+
+            Assert.ThrowsAny<Exception>(() =>
+            {
+                context.Connection.Open();
+                context.Connection.Close();
+            });
         }
     }
 }
